463a06ca5febca69fc9c385c82b40ea81e2fdcf7
Vitor Leal <vitorleal1@gmail.com>
2014-11-29 00:00:30 -0200

    add voice recognition

--- web/js/main.js
+++ web/js/main.js
@@ -1,25 +1,81 @@
 (function () {
-  $(document).on('ready', function () {
-    var socket = io.connect();
+  //Create a socket connection
+  var socket = io.connect();
 
+  //Defaul commands
+  var command = {
+    emit: function (action) {
+      socket.emit(action);
+      console.log(action);
+    },
+    start: function () {
+      command.emit('start');
+    },
+    end: function () {
+      command.emit('end');
+    },
+    up: function () {
+      command.emit('up');
+    },
+    down: function () {
+      command.emit('down');
+    }
+  };
+
+  //When document is ready
+  $(document).on('ready', function () {
+    //On socket connection open
     socket.on('open', function (data) {
-      console.log('open');
+      console.log('Open connection');
     });
 
+    //Handle clicks
     $('#start').on('click', function () {
-      socket.emit('start');
+      command.start();
     });
 
     $('#end').on('click', function () {
-      socket.emit('end');
+      command.end();
     });
 
     $('#up').on('click', function () {
-      socket.emit('up');
+      command.up();
     });
 
     $('#down').on('click', function () {
-      socket.emit('down');
+      command.down();
     });
+
+    //Talk to the drone
+    annyang.debug();
+    annyang.addCommands({
+      'drone ligar': command.start,
+      'drone desligar': command.end
+    });
+    annyang.setLanguage('pt-BR');
+    annyang.start();
+  });
+
+  //Key down controls
+  $(document).on('keydown', function (e) {
+    //On enter click
+    if (e.which === 13) {
+      command.start();
+    }
+
+    //On esc click
+    if (e.which === 27) {
+      command.end();
+    }
+
+    //On up arrow click
+    if (e.which === 38) {
+      command.up();
+    }
+
+    //On down arrow click
+    if (e.which === 40) {
+      command.down();
+    }
   });
 })();
